73278bf6e4b7f3e4c1b08ac739754eb17532e412
Dan Brown <roooms@gmail.com>
2016-07-06 18:12:51 +0100

    Add beam_decompile.erl escript

---
+++ scripts/beam_decompile.erl
@@ -0,0 +1,7 @@
+#!/usr/bin/env escript
+% -*- mode: erlang -*-
+
+%% usage: escript beam_decompile.erl filename.beam > filename.erl
+main([BeamFile]) ->
+    {ok,{_,[{abstract_code,{_,AC}}]}} = beam_lib:chunks(BeamFile,[abstract_code]),
+    io:fwrite("~s~n", [erl_prettypr:format(erl_syntax:form_list(AC))]).
