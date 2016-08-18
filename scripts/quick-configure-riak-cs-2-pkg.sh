fd0a4f8a37333e743ba03c357194ee82f91cf3b7
Dan Brown <roooms@gmail.com>
2016-08-18 11:05:23 +0100

    Skip setting iptables

--- scripts/quick-configure-riak-cs-2-pkg.sh
+++ scripts/quick-configure-riak-cs-2-pkg.sh
@@ -5,7 +5,7 @@ export riak_cs_ipaddr="0.0.0.0"
 export riak_cs_version="$(sudo riak-cs version)"
 
 # redirect port 80 to 8080 for local traffic
-sudo iptables --table nat --insert OUTPUT --protocol tcp --out-interface lo --dport 80 --jump REDIRECT --to-ports 8080 -v
+#sudo iptables --table nat --insert OUTPUT --protocol tcp --out-interface lo --dport 80 --jump REDIRECT --to-ports 8080 -v
 
 # riak configuration for riak-cs
 echo "buckets.default.allow_mult = true" | sudo tee -a /etc/riak/riak.conf
