3ee4d5ecaa5cdf4d085bffbe30104461eedec486
Dan Brown <roooms@gmail.com>
2016-09-02 08:57:47 +0100

    Add curl GET/PUT scripts

---
+++ scripts/curl_put.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+# defaults
+riak_ip="localhost"
+riak_port="8098"
+bucket_type="default"
+bucket="b1"
+first_key="001"
+last_key="100"
+value="test_value"
+
+usage()
+{
+    echo "Usage: $0 -s [riak_ip] -p [riak_port] -t [bucket_type] -b [bucket] -f [first_key] -l [last_key] -v [value]"
+}
+
+# get options
+while getopts "hs:p:t:b:f:l:v:" option; do
+    case $option in
+        h) 
+            usage
+            exit
+        ;;
+        s)
+            riak_ip=$OPTARG
+        ;;
+        p)
+            riak_port=$OPTARG
+        ;;
+        t)
+            bucket_type=$OPTARG
+        ;;
+        b)
+            bucket=$OPTARG
+        ;;
+        f)
+            first_key=$OPTARG
+        ;;
+        l)
+            last_key=$OPTARG
+        ;;
+        l)
+            value=$OPTARG
+        ;;
+    esac
+done
+
+# define bucket url based on Riak version
+if [ $bucket_type != "default" ]; then
+    riak_bucket_url="http://${riak_ip}:${riak_port}/types/${bucket_type}/buckets/${bucket}"
+else
+    riak_bucket_url="http://${riak_ip}:${riak_port}/buckets/${bucket}"
+fi
+
+# curl the bucket properties and check for a HTTP 200 response
+riak_http_check=$(curl --silent --head "${riak_bucket_url}/props" | head --lines 1 | grep "200")
+
+# run loop
+if [[ -n ${riak_http_check} ]]; then
+    for key in $(eval echo "{$first_key..$last_key}"); do
+        riak_key_url="${riak_bucket_url}/keys/${key}"
+        echo "[info] PUT ${riak_key_url}" \
+        && curl -XPUT "${riak_key_url}" -d "${value}"
+    done
+else
+    echo "[error] ${riak_bucket_url}/props did not return an HTTP 200 response"
+fi
