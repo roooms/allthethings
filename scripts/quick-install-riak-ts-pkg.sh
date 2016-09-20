57e488541ab9f85dfb0694327f85be2ded50d72a
Dan Brown <roooms@gmail.com>
2016-09-20 09:58:54 +0100

    Remove table creation from install script

--- scripts/quick-install-riak-ts-pkg.sh
+++ scripts/quick-install-riak-ts-pkg.sh
@@ -16,10 +16,3 @@ if [[ ${response} =~ ^([yY])$ ]]; then
 fi
 
 ${script_path}/install-riak-python-client.sh
-
-# initial table setup
-sudo su riak
-riak-admin bucket-type create GeoCheckin '{"props":{"table_def": "CREATE TABLE GeoCheckin (myfamily varchar not null, myseries varchar not null, time timestamp not null, weather varchar not null, temperature double, PRIMARY KEY ((myfamily, myseries, quantum(time, 15, 'm')), myfamily, myseries, time))"}}'
-riak-admin bucket-type activate GeoCheckin
-riak-admin bucket-type status GeoCheckin
-exit
