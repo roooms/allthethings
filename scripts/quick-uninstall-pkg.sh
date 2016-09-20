82c8b96d8ee09c9aff871442b5bcc37aa631c422
Dan Brown <roooms@gmail.com>
2016-09-20 11:20:00 +0100

    Add riak-ts to dpkg uninstall list

--- scripts/quick-uninstall-pkg.sh
+++ scripts/quick-uninstall-pkg.sh
@@ -4,7 +4,7 @@ sudo riak-cs stop
 sudo stanchion stop
 sudo riak stop
 
-sudo dpkg -P riak-cs stanchion riak riak-ee
+sudo dpkg -P riak-cs stanchion riak riak-ts riak-ee
 sudo rm -rfv /etc/riak/ /usr/lib/riak /var/lib/riak/
 sudo rm -rfv /etc/riak-cs/ /usr/lib/riak-cs/ /var/lib/riak-cs/
 sudo rm -rfv /etc/stanchion/ /usr/lib/stanchion/ /var/lib/stanchion/
